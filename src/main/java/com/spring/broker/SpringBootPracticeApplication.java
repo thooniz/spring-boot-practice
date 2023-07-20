06460c33b8c5fc0eefdd01c4f7d2468203fb69d1
thooniz <139753288+thooniz@users.noreply.github.com>
2023-07-20 16:18:27 +0900

    @Slf4j Logger 추가

--- src/main/java/com/spring/broker/SpringBootPracticeApplication.java
+++ src/main/java/com/spring/broker/SpringBootPracticeApplication.java
@@ -1,12 +1,15 @@
 package com.spring.broker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootPracticeApplication {
 
     public static void main(String[] args) {
+        log.info("Slf4j 테스트");
         SpringApplication.run(SpringBootPracticeApplication.class, args);
     }
 
