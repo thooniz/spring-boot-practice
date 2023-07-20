a8636e9fb088413ce5931906ee1be0857d31bca7
thooniz <139753288+thooniz@users.noreply.github.com>
2023-07-20 15:41:49 +0900

    spring-boot-practice 최초생성

---
+++ src/main/java/com/spring/broker/SpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPracticeApplication.class, args);
+    }
+
+}
